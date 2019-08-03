b0bdbbda12f9e911d90cf7ee82eb050cd2f49362
Rabee N. Moraga <rabmoree@gmail.com>
2019-08-03 13:22:35 +0800

    CI Activity Create new items

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,6 +56,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $route['default_controller'] = 'pages/view';
 // $route['(:any)'] = 'pages/view/$1';
 
+// $route['news/(:any)'] = 'news/view/$1';
+// $route['news'] = 'news';
+// $route['(:any)'] = 'pages/view/$1';
+// $route['default_controller'] = 'pages/view';
+
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
